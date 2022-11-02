513be091258963313aa5831f6e70e8fccdbdd84b
JLindwick <justin.christopher.russell@gmail.com>
2022-11-02 15:52:03 -0700

    first commit

--- zip up folder/server/routes/productManager.routes.js
+++
@@ -1,5 +0,0 @@
-const productManager = require('../controllers/productManager.controller');
-module.exports = function(app){
-    app.get('/api', productManager.index);
-    app.post('/api/product',productManager.createProduct)
-}
